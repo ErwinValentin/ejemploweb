4c50a8301d0bdecf848d301536a9fd5fd90f19a9
Erwin Gonzalez <erwinyux@hotmail.com>
2020-02-20 18:38:23 -0600

    first commit

---
+++ scirpt.js
@@ -0,0 +1,35 @@
+function guardarDatos(){
+    //Leer los valores del nombre y la edad 
+
+    //mostrar alerta si el nombre esta vacio
+
+    //mostrar alerta si la edad esta vacia o es 0 o menor
+
+    //guardar el nombre
+
+    //leer el nombre guardado
+
+    //basado en la edad crear un mensaje de bienvenida
+
+    //mostrar los mensajes y cambiarles los estilos
+    return false;
+}
+
+function guardarUsuario(nombre){
+    //simular el guardado de nombre usando el local storage
+}
+function obtenerNombre(){
+
+}
+function calcularSaludo(edad){
+    //cambiar el saludo si la edad es menor a 18
+}
+function resetearPagina(){
+    //borrar el nombre del local storage y refrescar la pagina
+}
+function imprimirPagina(){
+    //ocultar el formulario, luego llamar a imprimir pantalla, y finalmente mostrar el formulario otra vez
+}
+function mostrarPassword(){
+    //cambiar el tipo de input del campo password
+}
